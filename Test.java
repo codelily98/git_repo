a15cd5675b7a20b57eeb0054d1f470ad57d53951
codelily <coldelily98@naver.com>
2024-07-29 16:17:40 +0900

    clean push

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 class Test {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요 Git입니다.");
+<<<<<<< HEAD
+		System.out.println("반갑습니다 Git입니다.");
+	}
+=======
 		}
+>>>>>>> parent of f063a3e (Test.java version2)
 }
